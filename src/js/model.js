2d9a5e1caa060f851c1bc0935e8a4f59cc941a4f
Patryk <krasowski.p@outlook.com>
2023-07-27 19:05:37 +0200

    created controlSearchResults responsible for fetch search query

--- src/js/model.js
+++ src/js/model.js
@@ -3,6 +3,10 @@ import { getJSON } from './helpers'
 
 export const state = {
 	recipe: {},
+	search: {
+		query: '',
+		results: [],
+	},
 }
 
 export const loadRecipe = async id => {
@@ -20,6 +24,22 @@ export const loadRecipe = async id => {
 			servings: recipe.servings,
 			cookingTime: recipe.cooking_time,
 		}
+	} catch (error) {
+		throw error
+	}
+}
+
+export const loadSearchResults = async query => {
+	try {
+
+
+		state.search.query = query
+		const data = await getJSON(`${API_URL}?search=${query}`)
+
+		state.search.results = data.data.recipes.map(rec => {
+			return { id: rec.id, publisher: rec.publisher, image: rec.image_url, title: rec.title }
+		})
+		console.log(state.search.results);
 	} catch (error) {
 		throw error
 	}
