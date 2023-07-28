88d16ee7b20c94b74c61ae52be8bb7828ff38b8b
Patryk <krasowski.p@outlook.com>
2023-07-28 20:05:50 +0200

    created controlPagination responsible for render resultsView and paginationView

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import * as model from './model'
 import recipeView from './views/recipeView'
 import searchView from './views/searchView'
 import resultsView from './views/resultsView'
+import paginationView from './views/paginationView'
 
 const controlRecipes = async () => {
 	try {
@@ -27,12 +28,21 @@ const controlSearchResults = async () => {
 
 		// 2) load search results
 		await model.loadSearchResults(query)
-	
-		resultsView.render(model.getSearchResultsPage(1))
+
+		resultsView.render(model.getSearchResultsPage())
+
+		paginationView.render(model.state.search)
 	} catch (err) {}
 }
+
+const controlPagination = goToPage => {
+	resultsView.render(model.getSearchResultsPage(goToPage))
+	paginationView.render(model.state.search)
+}
+
 const init = () => {
 	recipeView.addHandlerRender(controlRecipes)
 	searchView.addHandlerSearch(controlSearchResults)
+	paginationView.addHandlerClick(controlPagination)
 }
 init()
