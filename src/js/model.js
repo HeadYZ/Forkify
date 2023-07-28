7044f5e1244dea9420a10d87a6b2f0fad3a4c963
Patryk <krasowski.p@outlook.com>
2023-07-28 20:04:51 +0200

    created getSearchResultPage responsible for render a certain number of recipes

--- src/js/model.js
+++ src/js/model.js
@@ -6,6 +6,7 @@ export const state = {
 	search: {
 		query: '',
 		results: [],
+		page: 1,
 		resultsPerPage: RES_PER_PAGE,
 	},
 }
@@ -43,7 +44,8 @@ export const loadSearchResults = async query => {
 	}
 }
 
-export const getSearchResultsPage = page => {
+export const getSearchResultsPage = (page = state.search.page) => {
+	state.search.page = page 
 	const start = (page - 1) * state.search.resultsPerPage //0
 	const end = page * state.search.resultsPerPage // 9
 	return state.search.results.slice(start, end)
